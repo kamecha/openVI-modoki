a59eb86d53f4a4e2fb6d203c704c4b17db389e5c
kamecha <masakikameyama81@gmail.com>
2024-02-05 22:58:52 +0900

    OpenAIの初期化を明示するよう変更

--- denops/openVI/main.ts
+++ denops/openVI/main.ts
@@ -2,10 +2,15 @@ import { Denops } from "./deps/denops.ts";
 import { GetAPIKey, InitializeOpenAI } from "./openai.ts";
 import { OpenPrompt } from "./prompt.ts";
 import { ChatStream, OpenChat } from "./chat.ts";
+import { OpenAI } from "./deps/openai.ts";
 
 export function main(denops: Denops) {
-  const openai = InitializeOpenAI(GetAPIKey());
+  let openai: OpenAI;
   denops.dispatcher = {
+    setup(): Promise<unknown> {
+      openai = InitializeOpenAI(GetAPIKey());
+      return Promise.resolve();
+    },
     async openPrompt(): Promise<unknown> {
       await OpenPrompt(denops);
       return Promise.resolve();
@@ -13,6 +18,11 @@ export function main(denops: Denops) {
     async sendPrompt(prompt: unknown): Promise<unknown> {
       const content = (prompt as string[]).join("\n");
       const chatBuffer = await OpenChat(denops);
+      if (!openai) {
+        console.error("OpenAI is not initialized");
+        console.error("Please run :VISetup to initialize OpenAI");
+        return Promise.resolve();
+      }
       await ChatStream(denops, openai, chatBuffer, content);
       return Promise.resolve();
     },
